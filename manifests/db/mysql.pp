7e5e4257eaa5f7f9cd40bfed75d2fe0801dadcb4
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2024-12-25 12:25:09 +0100

    add parameter for mysql db collate/charset

--- manifests/db/mysql.pp
+++ manifests/db/mysql.pp
@@ -8,17 +8,25 @@
 #   password for the user
 # @param host
 #   database host
+# @param collate
+#   set collate
+# @param charset
+#   set charset
 #
 class roundcube::db::mysql (
-  String $dbname = $roundcube::db::dbname,
-  String $dbuser = $roundcube::db::dbuser,
-  String $dbpass = $roundcube::db::dbpass,
-  String $host   = $roundcube::db::host,
+  String              $dbname  = $roundcube::db::dbname,
+  String              $dbuser  = $roundcube::db::dbuser,
+  String              $dbpass  = $roundcube::db::dbpass,
+  String              $host    = $roundcube::db::host,
+  Optional[String[1]] $collate = undef,
+  Optional[String[1]] $charset = undef,
 ) inherits roundcube::db {
   mysql::db { $dbname :
     user     => $dbuser,
     password => $dbpass,
     host     => $host,
     grant    => ['ALTER', 'CREATE', 'SELECT', 'INSERT', 'UPDATE', 'DELETE'],
+    collate  => $collate,
+    charset  => $charset,
   }
 }
